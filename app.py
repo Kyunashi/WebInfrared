a9ff0741eb527a428c28b025a3efeea64ad4b374
quentin <qlauterbach.dev@gmail.com>
2023-04-17 02:18:56 +0200

    Add Numpad that prints numbers on the console

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        button_num = request.form['buttonId']
+        print(f"Button {button_num} pressed!")
     return render_template('index.html')
 
 if __name__ == '__main__':
