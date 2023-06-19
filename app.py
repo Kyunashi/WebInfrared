7586fcdd2fd25c7168d81a00c3adccb062a8942f
quentin <qlauterbach.dev@gmail.com>
2023-06-19 18:40:58 +0200

    Add connection to lirc daemon

--- app.py
+++ app.py
@@ -1,13 +1,20 @@
 from flask import Flask, render_template, request
+import socket
+import lirc
 
 app = Flask(__name__)
+client = lirc.Client()
+  
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         button_num = request.form['buttonId']
         print(f"Button {button_num} pressed!")
+        print(client.version())
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True)
+    
+
