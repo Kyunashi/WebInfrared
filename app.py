6f6e9f402bee84fffeb61773bd0b5ef272579421
quentin <qlauterbach.dev@gmail.com>
2023-06-20 17:22:39 +0200

    Add Branch for Web part

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 from flask import Flask, render_template, request
-import lirc
 
 app = Flask(__name__)
-client = lirc.Client()
+
   
 
 
@@ -19,9 +18,6 @@ def index():
     if request.method == 'POST':
         button_num = request.form['buttonId']
         print(f"Button {button_num} pressed!")
-        print("Lirc Version: " + client.version())
-        client.send_once("beamer", "KEY_POWER")
-        print("Beamer Power Key Clicked")
     return render_template('index.html')
 
 if __name__ == '__main__':
